1be804544b05b50d233b84c830ea699ca4ef0650
Tobi Poel <tobipoel@gmail.com>
2017-12-12 11:42:12 +0100

    temporarily switches to staging.eyeem.com for font fetching

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -6,27 +6,27 @@ export default css`
 
   @font-face {
     font-family: 'Sailec Regular';
-    src: url('https://www.eyeem.com/node-static/fonts/sailec/SailecRegular.eot?#iefix');
-    src: url('https://www.eyeem.com/node-static/fonts/sailec/SailecRegular.eot?#iefix')
+    src: url('https://staging.eyeem.com/node-static/fonts/sailec/SailecRegular.eot?#iefix');
+    src: url('https://staging.eyeem.com/node-static/fonts/sailec/SailecRegular.eot?#iefix')
         format('eot'),
-      url('https://www.eyeem.com/node-static/fonts/sailec/SailecRegular.woff')
+      url('https://staging.eyeem.com/node-static/fonts/sailec/SailecRegular.woff')
         format('woff'),
-      url('https://www.eyeem.com/node-static/fonts/sailec/SailecRegular.ttf')
+      url('https://staging.eyeem.com/node-static/fonts/sailec/SailecRegular.ttf')
         format('truetype'),
-      url('https://www.eyeem.com/node-static/fonts/sailec/SailecRegular.svg#48d599a6-92b5-4d43-a4ac-8959f6971853')
+      url('https://staging.eyeem.com/node-static/fonts/sailec/SailecRegular.svg#48d599a6-92b5-4d43-a4ac-8959f6971853')
         format('svg');
   }
 
   @font-face {
     font-family: 'Sailec Bold';
-    src: url('https://www.eyeem.com/node-static/fonts/sailec/SailecBold.eot?#iefix');
-    src: url('https://www.eyeem.com/node-static/fonts/sailec/SailecBold.eot?#iefix')
+    src: url('https://staging.eyeem.com/node-static/fonts/sailec/SailecBold.eot?#iefix');
+    src: url('https://staging.eyeem.com/node-static/fonts/sailec/SailecBold.eot?#iefix')
         format('eot'),
-      url('https://www.eyeem.com/node-static/fonts/sailec/SailecBold.woff')
+      url('https://staging.eyeem.com/node-static/fonts/sailec/SailecBold.woff')
         format('woff'),
-      url('https://www.eyeem.com/node-static/fonts/sailec/SailecBold.ttf')
+      url('https://staging.eyeem.com/node-static/fonts/sailec/SailecBold.ttf')
         format('truetype'),
-      url('https://www.eyeem.com/node-static/fonts/sailec/SailecBold.svg#25e09910-ffc3-4fc4-b0d1-db9a95dface8')
+      url('https://staging.eyeem.com/node-static/fonts/sailec/SailecBold.svg#25e09910-ffc3-4fc4-b0d1-db9a95dface8')
         format('svg');
   }
 
