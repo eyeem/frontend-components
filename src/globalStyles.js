ee8c6957620eef45bfa20caa5715839007ec5a7b
Tobi Poel <tobipoel@gmail.com>
2017-12-12 15:06:52 +0100

    back to relative font paths for now

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -6,27 +6,21 @@ export default css`
 
   @font-face {
     font-family: 'Sailec Regular';
-    src: url('https://staging.eyeem.com/node-static/fonts/sailec/SailecRegular.eot?#iefix');
-    src: url('https://staging.eyeem.com/node-static/fonts/sailec/SailecRegular.eot?#iefix')
-        format('eot'),
-      url('https://staging.eyeem.com/node-static/fonts/sailec/SailecRegular.woff')
-        format('woff'),
-      url('https://staging.eyeem.com/node-static/fonts/sailec/SailecRegular.ttf')
-        format('truetype'),
-      url('https://staging.eyeem.com/node-static/fonts/sailec/SailecRegular.svg#48d599a6-92b5-4d43-a4ac-8959f6971853')
+    src: url('/node-static/fonts/sailec/SailecRegular.eot?#iefix');
+    src: url('/node-static/fonts/sailec/SailecRegular.eot?#iefix') format('eot'),
+      url('/node-static/fonts/sailec/SailecRegular.woff') format('woff'),
+      url('/node-static/fonts/sailec/SailecRegular.ttf') format('truetype'),
+      url('/node-static/fonts/sailec/SailecRegular.svg#48d599a6-92b5-4d43-a4ac-8959f6971853')
         format('svg');
   }
 
   @font-face {
     font-family: 'Sailec Bold';
-    src: url('https://staging.eyeem.com/node-static/fonts/sailec/SailecBold.eot?#iefix');
-    src: url('https://staging.eyeem.com/node-static/fonts/sailec/SailecBold.eot?#iefix')
-        format('eot'),
-      url('https://staging.eyeem.com/node-static/fonts/sailec/SailecBold.woff')
-        format('woff'),
-      url('https://staging.eyeem.com/node-static/fonts/sailec/SailecBold.ttf')
-        format('truetype'),
-      url('https://staging.eyeem.com/node-static/fonts/sailec/SailecBold.svg#25e09910-ffc3-4fc4-b0d1-db9a95dface8')
+    src: url('/node-static/fonts/sailec/SailecBold.eot?#iefix');
+    src: url('/node-static/fonts/sailec/SailecBold.eot?#iefix') format('eot'),
+      url('/node-static/fonts/sailec/SailecBold.woff') format('woff'),
+      url('/node-static/fonts/sailec/SailecBold.ttf') format('truetype'),
+      url('/node-static/fonts/sailec/SailecBold.svg#25e09910-ffc3-4fc4-b0d1-db9a95dface8')
         format('svg');
   }
 
