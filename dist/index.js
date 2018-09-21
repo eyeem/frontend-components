b8e0bbeaaab1f5b47a38372f9ef2fb45c84541d4
Eric Schaefer <eric@eyeem.com>
2018-09-21 11:13:12 +0200

    Fix fill rule

--- dist/index.js
+++ dist/index.js
@@ -3404,7 +3404,7 @@ function PaypalIcon(props) {
     },
     React.createElement(
       "g",
-      { "fill-rule": "evenodd", opacity: ".5" },
+      { fillRule: "evenodd", opacity: ".5" },
       React.createElement("path", {
         d: "M13.5 4.6c.2-1.4 0-2.4-.8-3.2C12 .4 10.4 0 8.5 0H3c-.3 0-.6.3-.7.7L0 15.4c0 .3.2.6.5.6H4l-.4 1.5c0 .2.2.5.4.5h3c.2 0 .5-.3.6-.6v-.2l.5-3.4.3-.2c0-.4.3-.6.6-.6h.4c2.7 0 5-1.2 5.5-4.5.2-1.4 0-2.5-.7-3.3-.2-.3-.4-.5-.7-.6",
         fill: "#ABDFF5"
