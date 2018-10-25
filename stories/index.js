92f92f8bf4fb760929ebb88c76571a9d36efe80a
Eric Schaefer <eric@eyeem.com>
2018-10-25 15:55:45 +0200

    Add knobs

--- stories/index.js
+++ stories/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import { storiesOf, addDecorator } from '@storybook/react';
+import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { withInfo } from '@storybook/addon-info';
+import { withInfo, setDefaults } from '@storybook/addon-info';
+import { withKnobs } from '@storybook/addon-knobs/react';
+import { selectV2 } from '@storybook/addon-knobs';
 
 import Button from '../src/components/button';
 import Input from '../src/components/input';
 
-addDecorator((story, context) => withInfo('common info')(story)(context));
+import { color, icon, size } from './knobs/button';
+
+// addon-info
+setDefaults({
+  header: false,
+  inline: true
+});
 
 storiesOf('Button', module)
-  .add('with text and plus icon', () => (
-    <Button size="small" color="green" icon="plus" onClick={action('clicked')}>
+  .addDecorator((story, context) => withInfo('common info')(story)(context))
+  .addDecorator(withKnobs)
+  .add('with text', () => (
+    <Button size="small" color="green" onClick={action('clicked')}>
       Hello Button
+    </Button>
+  ))
+  .add('with icon', () => (
+    <Button
+      size={selectV2(size.label, size.options, size.defaultValue)}
+      color={selectV2(color.label, color.options, color.defaultValue)}
+      icon={selectV2(icon.label, icon.options, icon.defaultValue)}
+    >
+      Button
     </Button>
   ))
   .add('with some emoji', () => (
---
+++ stories/knobs/button.js
@@ -0,0 +1,34 @@
+const color = {
+  label: 'Colors',
+  options: {
+    // load these from elsewhere...
+    red: 'red',
+    green: 'green'
+  },
+  defaultValue: 'red',
+  groupId: 'Button Options'
+};
+
+const icon = {
+  label: 'Icon',
+  options: {
+    // load these from elsewhere...
+    plus: 'plus',
+    edit: 'edit'
+  },
+  defaultValue: 'edit',
+  groupId: 'Button Options'
+};
+
+const size = {
+  label: 'Size',
+  options: {
+    // load these from elsewhere...
+    default: 'default',
+    large: 'large',
+    small: 'small',
+    minimal: 'minimal'
+  }
+};
+
+export { color, icon, size };
