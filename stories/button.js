d803cfb93207bd9d69a6cc95e7d75d47d55d7ef3
Stefan Neuberger <stefan.neuberger@eyeem.com>
2018-11-21 14:51:25 +0100

    This would centralise the Component. The StoryWrapper shows up in the 'Story Source' field in the story... so not ideal. I tried adding it to index.js directly without success.

--- stories/button.js
+++ stories/button.js
@@ -6,6 +6,7 @@ import { withInfo } from '@storybook/addon-info';
 import { knobs, getKnobTypeForProp } from './knobs/button';
 
 import Button from '../src/components/button';
+import StoryWrapper from '../src/Layout/StoryWrapper';
 
 const knobText = {
   label: 'Child text',
@@ -13,20 +14,22 @@ const knobText = {
 };
 
 storiesOf('Button', module)
+  .addDecorator(story => (<StoryWrapper>{story()}</StoryWrapper>))
   .addDecorator((story, context) => withInfo('')(story)(context))
-  .addDecorator(withKnobs)
+  .addDecorator(withKnobs)  
   .add('with controllable props', () => {
     const props = _.reduce(
       knobs,
       (result, value, key) => {
         const { knobFunction, args } = getKnobTypeForProp(key, value);
-
+        
         result[key] = knobFunction(...args);
-
+        
         return result;
       },
       {}
-    );
+      )
+      
 
     return (
       <Button {...props}>{text(knobText.label, knobText.defaultValue)}</Button>
