9cdd41339bd020a1295d37ab33265309ce36662f
Stefan Neuberger <stefan.neuberger@eyeem.com>
2019-01-04 16:26:56 +0100

    added basic checkbox tests

---
+++ src/components/checkbox/checkbox.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { render, cleanup, fireEvent } from 'react-testing-library';
+
+import Checkbox from './index';
+
+afterEach(cleanup);
+
+test('it renders default checkbox', () => {
+  const { container } = render(<Checkbox />);
+  const component = container.firstChild;
+  const checkboxElement = component.firstChild;
+  const labelElement = checkboxElement.nextSibling;
+  expect(checkboxElement.nodeName.toLowerCase()).toBe('input');
+  expect(checkboxElement.type).toBe('checkbox');
+  expect(labelElement.nodeName.toLowerCase()).toBe('label');
+  expect(component).toMatchSnapshot();
+});
+
+test('it accepts prop checked', () => {
+  const onChangeHandler = jest.fn();
+  const { container } = render(
+    <Checkbox
+      inputProps={{ checked: true, onChange: () => onChangeHandler() }}
+    />
+  );
+  const component = container.firstChild;
+  const checkboxElement = component.firstChild;
+  expect(checkboxElement.checked).toBe(true);
+  expect(component).toMatchSnapshot();
+});
+
+test('onChange is triggered properly', () => {
+  const onChangeHandler = jest.fn();
+  const { container, rerender } = render(
+    <Checkbox
+      inputProps={{ checked: true, onChange: () => onChangeHandler() }}
+    />
+  );
+  const component = container.firstChild;
+  const checkboxElement = component.firstChild;
+  fireEvent.click(checkboxElement);
+  expect(onChangeHandler).toHaveBeenCalledTimes(1);
+  expect(component).toMatchSnapshot();
+});
