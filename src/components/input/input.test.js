c08073f740b112854f3d06dfcc85ebc761a0de4b
Stefan Neuberger <stefan.neuberger@eyeem.com>
2019-01-04 14:42:33 +0100

    added basic Input tests

---
+++ src/components/input/input.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { render, cleanup } from 'react-testing-library';
+
+import Input from './index';
+
+const TEST_PLACEHOLDER = 'test placeholde';
+const TEST_VALUE = 'test value';
+const TEST_NAME = 'test name';
+
+const testInputProps = {
+  required: true,
+  value: TEST_VALUE,
+  onChange: () => this.onInputChangedHandler,
+  name: TEST_NAME,
+  placeholder: TEST_PLACEHOLDER
+};
+
+const onInputChangedHandler = () => 'changed';
+
+afterEach(cleanup);
+
+test('it renders default input', () => {
+  const { container } = render(<Input inputProps />);
+  const component = container.firstChild;
+  expect(component).toBeTruthy();
+  expect(component.firstChild).toHaveProperty('type');
+  expect(component.firstChild.type).toBe('text');
+  expect(component).toMatchSnapshot();
+});
+
+test('it accepts inputProps', () => {
+  const { container } = render(<Input inputProps={testInputProps} />);
+  const component = container.firstChild;
+  const inputElement = component.firstChild;
+  expect(inputElement).toHaveProperty('placeholder');
+  expect(inputElement.placeholder).toBe(TEST_PLACEHOLDER);
+  expect(inputElement).toHaveProperty('name');
+  expect(inputElement.name).toBe(TEST_NAME);
+  expect(inputElement).toHaveProperty('required');
+  expect(inputElement).toHaveProperty('type');
+  expect(inputElement.type).toBe('text');
+  expect(inputElement).toHaveProperty('value');
+  expect(inputElement.value).toBe(TEST_VALUE);
+  expect(component).toMatchSnapshot();
+});
