31a94ed24e64b497b312cd0d513ac94a3ca051f5
Eric Schaefer <eric@eyeem.com>
2019-02-07 13:56:07 +0100

    Accordion item fades

--- src/components/accordion/styledAccordion.js
+++ src/components/accordion/styledAccordion.js
@@ -1,7 +1,21 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+`;
 
 export const StyledAccordionItemBody = styled.div`
   display: ${props => (props.isShown ? 'block' : 'none')};
+  opacity: 0;
+  animation: ${fadeIn} ease 400ms;
+  animation-fill-mode: forwards;
+  animation-duration: 400ms;
   padding-bottom: 2rem;
   padding-top: 1rem;
 `;
--- stories/accordion.js
+++ stories/accordion.js
@@ -1,3 +1,4 @@
+// @flow
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs/react';
@@ -5,21 +6,40 @@ import { withInfo } from '@storybook/addon-info';
 
 import Accordion from '../src/components/accordion';
 import StyledText from '../src/components/text';
-
+import Buttonv2 from '../src/components/buttonv2';
 import StoryWrapper from '../src/Layout/StoryWrapper';
+
+import Themer from './themer';
+
+const themeDecorator = storyFn => <Themer>{storyFn()}</Themer>;
 
 const items = [
   {
     title: '1. Billing Information',
-    body: <StyledText>Body content</StyledText>
+    body: (
+      <div>
+        <StyledText>Body content</StyledText>
+        <Buttonv2 fill="outline">Save</Buttonv2>
+      </div>
+    )
   },
   {
     title: '2. Payment Method',
-    body: <StyledText>Body content</StyledText>
+    body: (
+      <div>
+        <StyledText>Body content</StyledText>
+        <Buttonv2 fill="outline">Save</Buttonv2>
+      </div>
+    )
   },
   {
     title: '3. Review',
-    body: <StyledText>Body content</StyledText>
+    body: (
+      <div>
+        <StyledText>Body content</StyledText>
+        <Buttonv2 fill="outline">Save</Buttonv2>
+      </div>
+    )
   }
 ];
 
@@ -27,6 +47,7 @@ storiesOf('Accordion', module)
   .addDecorator(story => <StoryWrapper>{story()}</StoryWrapper>)
   .addDecorator((story, context) => withInfo('')(story)(context))
   .addDecorator(withKnobs)
-  .add('with controllable props', () => (
-    <Accordion activeIndex={1} items={items} />
-  ));
+  .addDecorator(themeDecorator)
+  .add('default', () => <Accordion activeIndex={0} items={items} />)
+  .add('open next', () => <Accordion activeIndex={1} items={items} />)
+  .add('open last', () => <Accordion activeIndex={2} items={items} />);
