ef3a0a7a8ef708650b6590429c84cc055a522d49
Peter Willert <mastercain@web.de>
2019-11-12 21:05:29 +0100

    update cart icon again

--- src/components/icon/icons/cart.js
+++ src/components/icon/icons/cart.js
@@ -12,7 +12,7 @@ function CartIcon(props: { size?: number, className: string, id: string }) {
       viewBox="0 0 24 24">
       <path
         fill="none"
-        d="M7.5 7H6L4 21h16L18 7h-1.5m-9 0s.081-1.812 1.071-3.312c1.486-2.25 5.372-2.25 6.858 0C16.419 5.188 16.5 7 16.5 7m-9 0h9"
+        d="M0 2h3.5l1 12.5h15l3-9.5H3.821M9.5 20.5a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zm10.5 0a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0z"
         stroke="#fff"
         strokeWidth="2"
       />
